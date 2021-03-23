9654da64d4e7ec314fe7c9c85353e4d9a88f8734
jhs9497 <gustlr5043@naver.com>
2021-03-23 09:42:03 +0900

    made signup

---
+++ django/0323 django/0323_django_practice/accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import (
+    UserCreationForm,
+)
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('articles:index')
+
+    else: 
+        form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
