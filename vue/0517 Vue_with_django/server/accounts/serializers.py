a334c68cba52a6c732ca59a37d7bce57c8d0ab80
jhs9497 <gustlr5043@naver.com>
2021-05-17 16:54:00 +0900

    0517

---
+++ vue/0517 Vue_with_django/server/accounts/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True) # 시리얼라이즈는 되지만 응답에는 포함시키지 않겠다.
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
