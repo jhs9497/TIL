baf3c0a1fb71996a217f8708db38762d57709205
조현식 <gustlr5043@naver.com>
2021-07-02 19:53:49 +0900

    0702

--- 2021_하반기/개인 알고리즘 학습/JUNGOL/정올_2071파스칼삼각형.py
+++ 2021_하반기/개인 알고리즘 학습/JUNGOL/정올_2071파스칼삼각형.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split())
 
-answer = []
+Pascal = []
 
 for i in range(1, n+1):
     temp = [-1] * i
@@ -10,30 +10,41 @@ for i in range(1, n+1):
         elif j == i-1:
             temp[j] = 1
         else:
-            temp[j] = answer[i-2][j-1] + answer[i-2][j]
+            temp[j] = Pascal[i - 2][j - 1] + Pascal[i - 2][j]
 
-    answer.append(temp)
+    Pascal.append(temp)
 
 if m == 1:
-    for i in answer:
+    for i in Pascal:
         print(*i)
 
 elif m == 2:
     count = 0
-    for i in range(len(answer)-1, -1, -1):
-        pascal = ' '*count + ''
+    for i in range(len(Pascal) - 1, -1, -1):
+        row_pascal = ' '*count + ''
         for j in range(i+1):
-            pascal += str(answer[i][j]) + ' '
+            row_pascal += str(Pascal[i][j]) + ' '
         count += 1
-        print(pascal)
+        print(row_pascal)
 
 elif m == 3:
-    board = [[0]*n for _ in range(n)]
+    Pascal_copy = [[0] * n for _ in range(n)]
     for i in range(n):
         for j in range(i+1):
-            board[i][j] = answer[i][j]
+            Pascal_copy[i][j] = Pascal[i][j]
+
 
     New_pascal = [[0]*n for _ in range(n)]
     for i in range(len(New_pascal)):
         for j in range(len(New_pascal)):
-            print(board[i][j])
+            if i + j == n - 1:
+                New_pascal[i][j] = Pascal_copy[i][j]
+            else:
+                New_pascal[i][j] = Pascal_copy[n - 1 - j][n - 1 - i]
+
+    for i in range(len(New_pascal)):
+        row_pascal = ''
+        for j in range(len(New_pascal)):
+            if New_pascal[i][j] != 0:
+                row_pascal += str(New_pascal[i][j]) + ' '
+        print(row_pascal)
